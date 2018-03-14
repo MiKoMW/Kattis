173981144a2859073145eb4ca735c59618aac33e
Songbo Hu <s1647079@sms.ed.ac.uk>
2018-03-14 17:37:14 +0000

    Update answers.

---
+++ src/carrots.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by Mac on 2018/3/14.
+ */
+public class carrots {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            in.next();
+            System.out.print(in.next());
+            return;
+        }
+    }
+}
